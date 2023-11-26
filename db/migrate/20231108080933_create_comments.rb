05346bdaccbbedd5fbaa0ec54a2f4b4ee252506a
ryokatakahashi <ryonryon.xxxx@gmail.com>
2023-11-26 09:04:41 +0000

    [UPDATE]修正

--- db/migrate/20231108080933_create_comments.rb
+++ db/migrate/20231108080933_create_comments.rb
@@ -2,7 +2,7 @@ class CreateComments < ActiveRecord::Migration[6.1]
   def change
     create_table :comments do |t|
       t.bigint :post_id
-      t.big :user_id
+      t.bigint :user_id
       t.string :comment, null: false
 
       t.timestamps
